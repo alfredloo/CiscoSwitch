92008864ff4d6bb858d08ee0f016efe7720607bf
Alfred Loo <30677781+alfredloo@users.noreply.github.com>
2020-12-26 12:23:39 +0800

    Create Switch.bat

---
+++ Switch.bat
@@ -0,0 +1,22 @@
+Terminal length 0
+show clock
+show version
+show running 
+show redundancy
+show license all
+show switch detail
+show interface status
+show interfaces status err-disabled
+show interfaces trunk
+show vlan
+show spanning-tree
+show spanning-tree summary
+show etherchannel summary
+show power inline
+show stack-power detail
+show logging onboard switch active uptime detail
+show cdp neighbor
+show inventory
+show platform
+show environment all
+show clock
