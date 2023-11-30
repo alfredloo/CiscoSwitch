f62294db1dc6eb79bd21e1ba646287ebed579fc3
Alfred Loo <30677781+alfredloo@users.noreply.github.com>
2023-11-30 16:17:53 +0800

    Create Distribution Switch.bat
    Created for DS, seems the Onboard Failure logging differentiates this.

---
+++ Distribution Switch.bat
@@ -0,0 +1,25 @@
+Terminal length 0
+show clock
+show version
+show running 
+show redundancy
+show license all
+show switch detail
+show interface status
+show interface desc
+show interfaces status err-disabled
+show interfaces trunk
+show vlan
+show spanning-tree
+show spanning-tree summary
+show etherchannel summary
+show power inline
+show stack-power detail
+show logging onboard rp active uptime detail
+show cdp neighbor
+show cdp neighbor detail
+show inventory
+show platform
+show environment all
+show clock
+ter len 40
